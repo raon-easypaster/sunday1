f468db66515bcc84c26495964f93d0ad81eba3d0
이광복 <galeb76@gmail.com>
2025-11-16 23:36:02 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/sunday/',
+      base: '/sunday1/',
       server: {
         port: 3000,
         host: '0.0.0.0',
